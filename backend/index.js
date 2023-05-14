203bd643cc1d5b9ec3abd12494785764aadeb73d
Polina Shpak <uznavenko.polina@mail.ru>
2023-05-14 14:36:00 +0400

    deploy

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import { EmailSender } from "./sendEmail.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5001;
+app.post("/send", async (req, res) => {
+  try {
+    const {
+      name,
+      phone,
+      telegram,
+      dateStart,
+      dateEnd,
+      comments,
+      prod,
+      count,
+      price,
+    } = req.body;
+
+    EmailSender({
+      name,
+      phone,
+      telegram,
+      dateStart,
+      dateEnd,
+      comments,
+      prod,
+      count,
+      price,
+    });
+    res.json({ msg: "ok" });
+  } catch (error) {
+    res.status(404).json({ msg: "Error" });
+  }
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+export default app;
---
+++ backend/sendEmail.js
@@ -0,0 +1,56 @@
+import nodemailer from "nodemailer";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const Email = (options) => {
+  let transpoter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "0Auth2",
+      user: process.env.USER,
+      pass: process.env.PASSWORD,
+    },
+    host: "smtp.gmail.com",
+    port: 465,
+    tls: {
+      rejectUnauthorized: false,
+    },
+    secure: true,
+  });
+  transpoter.sendMail(options, (err, info) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("sent email");
+    }
+  });
+};
+
+export const EmailSender = ({
+  name,
+  phone,
+  telegram,
+  dateStart,
+  dateEnd,
+  comments,
+  prod,
+  count,
+  price,
+}) => {
+  const options = {
+    from: process.env.USER,
+    to: process.env.USER,
+    subject: "ЗАКАЗ",
+    html: `    <p>FullName: <b>${name}</b></p>
+    <p>Phone: <b>${phone}</b></p>
+    <p>Telegram: <b>${telegram}</b></p>
+    <p>dateStart: <b>${dateStart}</b></p>
+    <p>dateEnd: <i>${dateEnd}</i></p>
+    <p>comments: <i>${comments}</i></p>
+    <p>prod: <i>${prod}</i></p>
+    <p>count: <i>${count}</i></p>
+    <p>price: <i>${price}</i></p>`,
+  };
+  Email(options);
+};
