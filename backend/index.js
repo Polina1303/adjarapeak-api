40fd783d5d761f649ccbeb2e882bdc3d294ca236
Polina Shpak <uznavenko.polina@mail.ru>
2024-02-13 15:07:02 +0400

    fix

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ import { EmailSender } from "./sendEmail.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://www.adjarapeak.ge",
+  })
+);
 
 const PORT = process.env.PORT || 5001;
 app.post("/send", async (req, res) => {
