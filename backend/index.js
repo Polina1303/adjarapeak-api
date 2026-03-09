620565395265aece11ddcb2c5a37a02fef454438
Polina <uznavenko.polina@mai.ru>
2026-03-09 13:59:34 +0400

    fix

--- backend/index.js
+++ backend/index.js
@@ -143,7 +143,7 @@ app.post("/send", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 console.log("ENV PORT =", process.env.PORT);
 
